7c7d2a65199d4a782db469ce1a88ddc019a82ba9
Piyush-kag <piyush.kag@innogent.com>
2023-09-20 11:05:15 +0530

    Created new class Book

---
+++ src/main/java/com/springcore/Inno_spring1/Book.java
@@ -0,0 +1,69 @@
+package com.springcore.Inno_spring1;
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private int quantity;
+
+    public Book(String isbn, String title, String author, int quantity) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.quantity = quantity;
+    }
+
+
+
+    public String getIsbn() {
+		return isbn;
+	}
+
+
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+
+	@Override
+    public String toString() {
+        return "Book [ISBN=" + isbn + ", Title=" + title + ", Author=" + author + ", Quantity=" + quantity + "]";
+    }
+}
